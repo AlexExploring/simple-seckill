6a5be868297a33c126ae8a720bf4779fd5ff0b63
zhxxltx <3136713504@qq.com>
2022-07-23 20:15:55 +0800

    基本秒杀功能

--- src/main/java/com/zhx/controller/GoodsController.java
+++ src/main/java/com/zhx/controller/GoodsController.java
@@ -1,10 +1,6 @@
 package com.zhx.controller;
 
 import com.zhx.pojo.TUser;
-import com.zhx.service.TGoodsService;
-import com.zhx.service.TUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,15 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("goods")
 public class GoodsController {
-
-    @Autowired
-    private TGoodsService tGoodsService;
-
-    @Autowired
-    private TUserService tUserService;
-
-    @Autowired
-    private RedisTemplate redisTemplate;
 
     @GetMapping(value = "/toList")
     public String toList(Model model,TUser user) {
