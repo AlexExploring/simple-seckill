00a995fbfc08ab88b36a3c1962bfa1bfef508c1c
zhxxltx <3136713504@qq.com>
2022-07-24 18:26:59 +0800

    基本秒杀功能

--- src/main/java/com/zhx/controller/GoodsController.java
+++ src/main/java/com/zhx/controller/GoodsController.java
@@ -3,16 +3,23 @@ package com.zhx.controller;
 import com.zhx.pojo.TUser;
 import com.zhx.service.TGoodsService;
 import com.zhx.service.TUserService;
+import com.zhx.vo.DetailVo;
 import com.zhx.vo.GoodsVo;
+import com.zhx.vo.RespBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 商品
@@ -30,43 +37,75 @@ public class GoodsController {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    @RequestMapping(value = "/toList")
+    @Autowired
+    private ThymeleafViewResolver thymeleafViewResolver;
+
+    @RequestMapping(value = "/toList", produces = "text/html;charset=utf-8")
+    @ResponseBody
     public String toList(Model model, TUser user, HttpServletRequest request, HttpServletResponse response) {
-        model.addAttribute("user",user);
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        String html = (String) valueOperations.get("goodsList");
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+
+        model.addAttribute("user", user);
         model.addAttribute("goodsList", tGoodsService.findGoodsVo());
-        return "goodsList";
+
+        WebContext webContext = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goodsList", webContext);
+        if (!StringUtils.isEmpty(html)) {
+            valueOperations.set("goodsList", html, 60, TimeUnit.SECONDS);
+        }
+        return html;
     }
 
-    @RequestMapping(value = "/toDetail/{goodsId}")
-    public String toDetail2(Model model, TUser user, @PathVariable Long goodsId) {
+    @RequestMapping(value = "/detail/{goodsId}", produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String toDetail(Model model,TUser user, @PathVariable Long goodsId,
+                           HttpServletRequest request,HttpServletResponse response) {
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        String html = (String) valueOperations.get("goodsDetails:"+goodsId);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
         model.addAttribute("user",user);
 
         GoodsVo goodsVo = tGoodsService.findGoodsVoByGoodsId(goodsId);
-        model.addAttribute("goods",goodsVo);
         Date startDate = goodsVo.getStartDate();
         Date endDate = goodsVo.getEndDate();
         Date nowDate = new Date();
-
         //秒杀状态
-        int secKillStatus = 0;
+        int seckillStatus = 0;
         //秒杀倒计时
         int remainSeconds = 0;
+
         if (nowDate.before(startDate)) {
             //秒杀还未开始0
             remainSeconds = (int) ((startDate.getTime() - nowDate.getTime()) / 1000);
         } else if (nowDate.after(endDate)) {
             //秒杀已经结束
-            secKillStatus = 2;
+            seckillStatus = 2;
             remainSeconds = -1;
         } else {
             //秒杀进行中
-            secKillStatus = 1;
+            seckillStatus = 1;
             remainSeconds = 0;
         }
+//        model.addAttribute("remainSeconds", remainSeconds);
+//        model.addAttribute("goods", goodsVo);
+//        model.addAttribute("seckillStatus", seckillStatus);
 
-        model.addAttribute("secKillStatus",secKillStatus);
-        model.addAttribute("remainSeconds",remainSeconds);
-
-        return "goodsDetail";
+//        WebContext webContext = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+//        html = thymeleafViewResolver.getTemplateEngine().process("goodsDetail", webContext);
+//        if (!StringUtils.isEmpty(html)) {
+//            valueOperations.set("goodsDetail:" + goodsId, html, 60, TimeUnit.SECONDS);
+//        }
+        DetailVo detailVo = new DetailVo();
+        detailVo.setTUser(user);
+        detailVo.setGoodsVo(goodsVo);
+        detailVo.setRemainSeconds(remainSeconds);
+        detailVo.setSecKillStatus(seckillStatus);
+        return RespBean.success(detailVo);
     }
 }
